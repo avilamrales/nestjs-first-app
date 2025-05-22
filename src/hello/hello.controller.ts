f3e656277db8a817efb6bbe87deb84580dc18b4c
avilamrales <jesusavilamrales@protonmail.com>
2025-05-22 17:57:24 -0600

    Learning about HTTP status and how to use it with @HttpCode

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller()
 export class HelloController {
-  @Get('/')
+  @Get()
   index(@Req() request: Request, @Res() response: Response) {
     response.status(200).send({
       message: 'Hello World',
     });
+  }
+
+  @Get('hello')
+  @HttpCode(201)
+  getHello() {
+    return {
+      message: 'Hello from hello endpoint',
+    };
+  }
+
+  @Get('notfound')
+  @HttpCode(404)
+  notFoundPage() {
+    return {
+      message: 'Page not found',
+    };
+  }
+
+  @Get('error')
+  @HttpCode(500)
+  errorPage() {
+    return { message: 'Error page!!' };
   }
 }
