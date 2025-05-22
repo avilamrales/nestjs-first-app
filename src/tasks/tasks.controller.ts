3d9c98bf19213d24c93b6cbcf9df3dfddc98c93f
avilamrales <jesusavilamrales@protonmail.com>
2025-05-22 08:49:50 -0600

    Learning about services and how to create them

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller({})
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get('/tasks')
   getAllTasks() {
-    return 'Obteniendo todas las tareas';
+    return this.tasksService.getAllTasks();
   }
 }
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TasksService {
+  getAllTasks() {
+    return ['task1', 'task2', 'task3'];
+  }
+}
