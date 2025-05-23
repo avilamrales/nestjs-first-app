7b4a7ff7ebb6b30a0759ed050718464a51b2de8a
avilamrales <jesusavilamrales@protonmail.com>
2025-05-22 23:49:27 -0600

    Learning about the command "nest generate resources" and how to use it

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { HelloController } from './hello/hello.controller';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [TasksModule, AuthModule, UsersModule, ProjectsModule],
+  imports: [TasksModule, AuthModule, UsersModule, ProjectsModule, PaymentsModule],
   controllers: [HelloController],
 })
 export class AppModule {}
